3a39ea4b655b7b9759e17583090b7a20f1859b4e
Raja <rajanayakaa52@gmai.com>
2025-06-06 14:30:38 +0530

    Day6-this keyword

---
+++ Days/day6_this_keyword/Main.java
@@ -0,0 +1,8 @@
+package day6_this_keyword;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Ravi");
+        System.out.println("Person's name: " + person.name);
+    }
+}
---
+++ Days/day6_this_keyword/Person.java
@@ -0,0 +1,9 @@
+package day6_this_keyword;
+
+class Person {
+    String name;
+    
+    Person(String name) {
+        this.name = name;  // this refers to current instance variable
+    }
+}  
